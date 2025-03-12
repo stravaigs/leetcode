836df8209b1a299dee1b457c054e49efcb6e753e
Cutty-sark <cutty-s4rk@protonmail.com>
2025-03-12 22:35:49 +0000

    1684 wip

---
+++ 1684 Count the Number of Consistent Strings.c
@@ -0,0 +1,18 @@
+
+
+int countConsistentStrings(char * allowed, char ** words, int wordsSize){
+//allowed - char. words - array. con - 
+
+// use \n to break
+int con = 0
+for (int i = 0; i < sizeof(allowed) / sizeof(allowed[0]); i++)
+{
+    for (int j = 0; j < strlen(allowed[i]) - 1; j++)
+…        if (count == wordSize)
+        {
+            con++;
+        }
+    }
+}
+return con;
+}
